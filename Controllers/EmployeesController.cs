71365b662ff388bdb4bfb7333dfad0f992275440
jimutGitHub <jimutGitHub@users.noreply.github.com>
2019-12-05 09:30:23 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleDataAccess;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using(SampleDBEntities dbEntities = new SampleDBEntities())
+            {
+                return dbEntities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (SampleDBEntities dbEntities = new SampleDBEntities())
+            {
+                return dbEntities.Employees.FirstOrDefault(x => x.Id == id);
+            }
+        }
+    }
+}
